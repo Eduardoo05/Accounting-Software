fd698a2f0799ba756c14d42e5e72c14b7a17c4f2
Elvin Eduardo <franco193@hotmail.com>
2024-03-11 23:54:56 -0400

    se agregaron las hojas de BalanceGeneral y DocumentosFuentes

---
+++ src/pages/BalanceGeneral.jsx
@@ -0,0 +1,6 @@
+
+export const BalanceGeneral = () => {
+  return (
+    <div>BalanceGeneral</div>
+  )
+}
---
+++ src/pages/DocumentosFuentes.jsx
@@ -0,0 +1,6 @@
+
+export const DocumentosFuentes = () => {
+  return (
+    <div>DocumentosFuentes</div>
+  )
+}
